c281bf78a6c19a88ebe0ab05a2f36e4afa6df185
Michael Geramb <lasso_riefen0m@icloud.com>
2025-09-21 17:00:57 +0200

    Basic implementation (WIP)

--- src/NukiModule.cpp
+++ src/NukiModule.cpp
@@ -1,5 +1,41 @@
 #include "NukiModule.h"
+#include "NukiConstants.h"
 
+
+uint32_t deviceId = 2020001;
+std::string deviceName = "frontDoor";
+NukiLock::NukiLock* nukiLock = nullptr;
+
+
+
+void getConfig()
+{
+    NukiLock::Config config;
+    if (nukiLock->requestConfig(&config) == 1)
+    {
+        logDebug("Nuki", "Name: %s", config.name);
+    }
+    else
+    {
+        logDebug("Nuki", "getConfig failed");
+    }
+}
+
+bool NukiModule::keyTurnerState()
+{
+    uint8_t result = nukiLock->requestKeyTurnerState(&retrievedKeyTurnerState);
+    if (result == 1)
+    {
+        logDebug("Nuki", "Bat crit: %d, Bat perc:%d lock state: %d %d:%d:%d",
+                 nukiLock->isBatteryCritical(), nukiLock->getBatteryPerc(), retrievedKeyTurnerState.lockState, retrievedKeyTurnerState.currentTimeHour,
+                 retrievedKeyTurnerState.currentTimeMinute, retrievedKeyTurnerState.currentTimeSecond);
+    }
+    else
+    {
+        logDebug("Nuki", "cmd failed: %d", result);
+    }
+    return result;
+}
 
 const std::string NukiModule::name()
 {
@@ -20,15 +56,63 @@ const std::string NukiModule::version()
 #endif
 }
 
+void NukiModule::setup(bool configured)
+{
+    logDebugP("Start Bluetooth Scanner");
+    scanner = new BleScanner::Scanner();
+    scanner->initialize();
+    nukiLock = new NukiLock::NukiLock(deviceName, deviceId);
+    nukiLock->setEventHandler(nullptr);
+    
+    nukiLock->registerBleScanner(scanner);
+    nukiLock->initialize();
+}
+
+void NukiModule::loop(bool configured)
+{
+    if (scanner != nullptr)
+        scanner->update();
+    if (nukiLock != nullptr)
+    {
+        if (!nukiLock->isPairedWithLock())
+        {
+            if (nukiLock->pairNuki() == Nuki::PairingResult::Success)
+            {
+                log_d("paired");
+                nukiLock->setEventHandler(&notifyHandler);
+
+                getConfig();
+            }
+        }
+        if (notifyHandler.notified)
+        {
+            notifyHandler.notified = false;
+            if (keyTurnerState())
+            {
+               
+            }
+        }
+    }
+}
+
 void NukiModule::showHelp()
 {
 }
 
 bool NukiModule::processCommand(const std::string cmd, bool diagnoseKo)
 {
-    
+    if (cmd == "nuki unlock")
+    {
+        nukiLock->lockAction(NukiLock::LockAction::Unlock);
+        return true;
+    }
+    if (cmd == "nuki lock")
+    {
+        nukiLock->lockAction(NukiLock::LockAction::Lock);
+        return true;
+    }
+
     return false;
 }
- 
 
 NukiModule openknxNukiModule;
--- src/NukiModule.h
+++ src/NukiModule.h
@@ -1,11 +1,34 @@
 #pragma once
 #include "OpenKNX.h"
+#include "BleScanner.h"
+#include "NukiConstants.h"
+#include "NukiLock.h"
+
+class NukiNotifyHandler : public Nuki::SmartlockEventHandler
+{
+  public:
+    bool notified = false;
+  public:
+    void notify(Nuki::EventType eventType) override
+    {
+        notified = true;
+    }
+};
 
 class NukiModule : public OpenKNX::Module
 {
+  private:
+    NukiNotifyHandler notifyHandler = NukiNotifyHandler();
+    NukiLock::KeyTurnerState retrievedKeyTurnerState;
+
+    BleScanner::Scanner* scanner = nullptr;
+    
+    bool keyTurnerState();
   public:
     const std::string name() override;
     const std::string version() override;
+    void setup(bool configured) override;
+    void loop(bool configured) override;
     void showInformations() override;
     void showHelp() override;
     bool processCommand(const std::string cmd, bool diagnoseKo) override;
